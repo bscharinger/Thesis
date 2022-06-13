8a75206c175323625b3b2a7b52f604ee2e67a325
bscharinger <scharinger@student.tugraz.at>
2022-06-13 18:56:58 +0200

    Changed helper_funcs

--- helper_funcs.py
+++ helper_funcs.py
@@ -18,6 +18,7 @@ def jaccard_distance(y_true, y_pred):
 def dice_coef(y_true, y_pred, smooth=1):
 
     """
+    Calculate dice coefficient
     Dice = (2*|X & Y|)/ (|X|+ |Y|)
          =  2*sum(|A*B|)/(sum(A^2)+sum(B^2))
     ref: https://arxiv.org/pdf/1606.04797v1.pdf
@@ -27,9 +28,21 @@ def dice_coef(y_true, y_pred, smooth=1):
 
 
 def dice_coef_loss(y_true, y_pred):
+    """
+    Calculates the dice loss
+    :param y_true: True label
+    :param y_pred: Predicted Label
+    :return: Dice loss
+    """
     return 1-dice_coef(y_true, y_pred)
 
 def aorta_cc3d(input):
+    """
+    Does a connected component analysis in an input image and outputs the second biggest
+    connected component (Biggest one is background)
+    :param input: Segmented input image
+    :return: Second biggest connected component
+    """
     conn_comps = cc3d.connected_components(input)
     labels_out = conn_comps.reshape((1, -1))
     labels_out = labels_out[0, :]
@@ -45,23 +58,14 @@ def aorta_cc3d(input):
     output = labels_out * (labels_out == idx_)
     return output
 
-def resizing(label):
-    a,b,c=label.shape
-    resized_data = zoom(label,(256/a,256/b,128/c),order=2, mode='constant')
+def resizing(data):
+    """
+    Resize a 3d Image to 256x256x128 voxels
+    :param label:
+    :return: resized data
+    """
+    a,b,c=data.shape
+    resized_data = zoom(data,(256/a,256/b,128/c),order=2, mode='constant')
     return resized_data
 
 
-def skull_id(labels_out):
-    labels_out=labels_out.reshape((1,-1))
-    labels_out = labels_out[0,:]
-    label = np.unique(labels_out)
-    hist, bin_edges = np.histogram(labels_out, bins=label)
-    hist = np.ndarray.tolist(hist)
-    hist_ = hist
-    hist_ = np.array(hist_)
-    hist.sort(reverse=True)
-    idx = (hist_ == hist[1])
-    idx = idx+1-1
-    idx_ = np.sum(idx*label[0:len(idx)])
-    print('idx',idx_)
-    return idx_
